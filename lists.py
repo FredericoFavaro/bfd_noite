8f92a7f505d27f839acb8501d7196da2bf54e3af
Frederico Fávaro <fredericofavaro@gmail.com>
2025-08-15 14:09:23 -0300

    primeiro commit

---
+++ lists.py
@@ -0,0 +1,63 @@
+frutas = ["banana","manga", "laranja", "jaca", "jaca"]
+
+# salada_de_fruta = frutas
+
+# print(frutas)
+# print(salada_de_fruta)
+# salada_de_fruta.append("melao")
+# print("---")
+# print(id(frutas))
+# print(id(salada_de_fruta))
+# print("---")
+# print(frutas)
+# print(salada_de_fruta)
+
+# print(frutas[:])
+# print(frutas)
+temperos = ["pimenta", "sal"]
+# frutas += temperos
+# frutas.insert(5, "limao")
+# frutas.append("jaca")
+# fruta = "manga"
+
+#### REMOVER ITENS
+##i frutas[1]
+
+#frutas.pop(1)
+## nt(frutas.clear())
+
+#### ordenar listas
+
+#print(sorted(frutas))
+#frutas.sort(reverse=True)
+#print(frutas)
+
+fruta = "morango"
+
+morango_do_amor = fruta
+
+#print(morango_do_amor)
+
+# print(id(fruta))
+# print(id(morango_do_amor))
+# fruta = "molango"
+# print(id(fruta))
+# print(id(morango_do_amor))
+
+##### COPIAR
+salada_de_fruta = ["maça"]
+print(frutas)
+idx_jaca = frutas.index("jaca")
+frutas.pop(idx_jaca)
+print(frutas)
+
+#print(salada_de_fruta)
+# for fruta in frutas:
+#     salada_de_fruta.append(fruta)
+#     print(fruta)
+#     print(salada_de_fruta)
+
+#print(frutas.count("jaca"))
+
+
+
---
+++ teste.py
@@ -0,0 +1,9 @@
+linhas = 3
+colunas = 3
+
+matriz = []
+for i in range(linhas):
+    linha = []
+    for j in range(colunas):
+        linha.append(0)
+    matriz.append(linha)
